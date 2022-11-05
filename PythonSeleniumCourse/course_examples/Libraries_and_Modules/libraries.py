2d0c1b2b2a68df7a3022228fce970f0a05468427
Erhan  YILMAZ <eyilmaz@airspan.com>
2022-11-06 00:39:33 +0300

    libraries

---
+++ PythonSeleniumCourse/course_examples/Libraries_and_Modules/libraries.py
@@ -0,0 +1,31 @@
+# Some Standard Libraries
+# - datetime
+# - time
+# - random
+# - tempfile
+# - pdb  : debugger
+# - os
+# - csv  : sv files
+# - shutil
+# - argparse : script
+# - subprocess : unix command
+# - urllib
+# - pprint  : pretty print
+#
+# -----------------------------------------------------------
+# Q1: Is there a library to interact with browsers?
+# A1: Yes, It is called 'Selenium WebDriver'
+# $ pipe install selenium
+
+# Q2: Is there a library to make http requests?
+# A2: Yes, It is called 'requests'
+# $ pipe install requests
+
+# Q3: Is there a library to work with Amazon's s3?
+# A3: Yes, It is called 'boto3'
+# $ pipe install boto3
+
+
+
+
+
---
+++ PythonSeleniumCourse/course_examples/Libraries_and_Modules/random.py
@@ -0,0 +1,18 @@
+# example of 'random' module
+
+import random
+
+my_num = random.randint(100, 200)
+my_num = random.randrange(24)
+
+my_list = ['aaa', 'bbb', 'ccc', 'ddd']
+
+# randomize a list
+random.shuffle(my_list)
+random.choice(my_list)
+
+
+
+
+
+
