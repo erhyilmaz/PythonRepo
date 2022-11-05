34c654669179487b843e8dee2f8fb7eae697a2c5
Erhan  YILMAZ <eyilmaz@airspan.com>
2022-11-06 00:44:38 +0300

    libraries

--- PythonSeleniumCourse/course_examples/Libraries_and_Modules/random_lib_ex.py
+++ PythonSeleniumCourse/course_examples/Libraries_and_Modules/random_lib_ex.py
@@ -3,14 +3,16 @@
 import random
 
 my_num = random.randint(100, 200)
+print(my_num)
 my_num = random.randrange(24)
-
+print(my_num)
 my_list = ['aaa', 'bbb', 'ccc', 'ddd']
 
 # randomize a list
 random.shuffle(my_list)
-random.choice(my_list)
-
+print(my_list)
+my_list_ch = random.choice(my_list)
+print(my_list_ch)
 
 
 
