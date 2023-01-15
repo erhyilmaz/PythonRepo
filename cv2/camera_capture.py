df4a0f07783b5da11799ba1afda4d3618024717b
Erhan  YILMAZ <eyilmaz@airspan.com>
2023-01-16 01:45:26 +0300

    - cv2 example: MediaPipe library for the pose points: https://google.github.io/mediapipe/solutions/pose

--- cv2/camera_capture.py
+++ cv2/camera_capture.py
@@ -3,11 +3,12 @@ import numpy as np
 import math
 from cvzone.PoseModule import PoseDetector
 
+# live video capture
 cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture('train.mp4')
 pd = PoseDetector(detectionCon=0.70, trackCon=0.70)
 
-class angleFinder:
+
+class AngleFinder:
     def __init__(self, lmlist, p1, p2, p3, p4, p5, p6, drawpoints):
         self.lmlist = lmlist
         self.p1 = p1
@@ -35,10 +36,10 @@ class angleFinder:
             x6, y6 = point6[1:-1]
 
             leftAngle = math.degrees(math.atan2(y3-y2, x3-x2) -
-                                     math.atan2(y1-y2, x1-x2) )
+                                     math.atan2(y1-y2, x1-x2))
 
-            rightAngle = math.degrees(math.atan2(y6 - y5, x6 - x5) -
-                                      math.atan2(y1 - y2, x1 - x2))
+            rightAngle = math.degrees(math.atan2(y6-y5, x6-x5) -
+                                      math.atan2(y4-y5, x4-x5))
 
             leftAngle  = int(np.interp(leftAngle, [-170, 180], [100, 0]))
             rightAngle = int(np.interp(rightAngle, [-50, 20], [100, 0]))
@@ -66,28 +67,63 @@ class angleFinder:
                 cv2.line(img, (x5, y5), (x6, y6), (0, 0, 255), 4)
                 cv2.line(img, (x1, y1), (x4, y4), (0, 0, 255), 4)
 
-            return ([leftAngle, rightAngle])
+            return [leftAngle, rightAngle]
 
+
+score = 0
+direction = 0
 
 while True:  # cap.isOpened():
     ret, img = cap.read()
-    # img = cv2.resize(img, (640, 480), interpolation = cv2.INTER_AREA)
+    img = cv2.resize(img, (640, 480))
     pd.findPose(img, draw=0)
+
     lmlist, bbox = pd.findPosition(img, draw=0)
 
-    angles = angleFinder(lmlist, 11, 13, 15, 12, 14, 16, drawpoints=1)
+    angles = AngleFinder(lmlist, 11, 13, 15, 12, 14, 16, drawpoints=1)
     angle = angles.angle()
-
     left, right = angle[0:]
-    print(left, right)
+
+    # print(left, right)
+
+    if left >= 90 and right >= 90:
+        if direction == 0:
+            score += 0.5
+            direction = 1
+
+    if left <= 70 and right <= 70:
+        if direction == 1:
+            score += 0.5
+            direction = 0
+
+    # print(score)
+
+    rightVal = np.interp(right, [0, 100], [400, 200])
+    leftVal = np.interp(left, [0, 100], [400, 200])
+
+    # score
+    cv2.rectangle(img, (0, 0), (120, 120), (255, 0, 0), -1)
+    cv2.putText(img, str(int(score)), (1, 70), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1.6, (0, 0, 255), 6)
+
+    # right hand
+    cv2.putText(img, 'R', (24, 194), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0), 5)
+    cv2.rectangle(img, (8, 200), (50, 400), (0, 255, 255), 5)
+    cv2.rectangle(img, (8, int(rightVal)), (50, 400), (0, 255, 0), -1)
+
+    # left hand
+    cv2.putText(img, 'L', (604, 195), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0), 5)
+    cv2.rectangle(img, (582, 200), (632, 400), (0, 255, 255), 5)
+    cv2.rectangle(img, (582, int(leftVal)), (632, 400), (0, 255, 0), -1)
+
+    value_right = np.interp(right, [0, 100], [0, 100])
+    value_left = np.interp(left, [0, 100], [0, 100])
+    if value_left >= 70:
+        cv2.rectangle(img, (582, int(leftVal)), (632, 400), (0, 0, 255), -1)
+    if value_right >= 70:
+        cv2.rectangle(img, (8, int(rightVal)), (50, 400), (0, 0, 255), -1)
+
 
     # point = lmlist[12]  # check MediaPipe library for the pose points: https://google.github.io/mediapipe/solutions/pose
-    #print(lmlist)
-    # print(point)
-
-    # x,y,h, w = point
-    # cv2.circle(img, (y,h), 10, (255,0,0), 5)
 
     cv2.imshow('frame', img)
     cv2.waitKey(1)
-
